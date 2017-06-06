6fc5822248ed372bffb479f47756d85d0ce55e3c
anne <richardson.ae@gmail.com>
2017-06-06 16:11:10 -0500

    Specs for contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,10 @@
 class Contact < ApplicationRecord
   has_many :phones
+
+  validates :firstname, :lastname, :email, presence: true
+  validates :email, uniqueness: true
+
+  def name
+    "#{firstname} #{lastname}"
+  end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  it "is valid with a firstname, lastname, and email" do
+    contact = Contact.new(
+      firstname: 'Murray',
+      lastname: 'Hewitt',
+      email: 'murray@email.com')
+    expect(contact).to be_valid
+  end
+
+  it "is invalid without a firstname" do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
+
+  it "is invalid without a lastname" do
+    contact = Contact.new(lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
+
+  it "is invalid without a email" do
+    contact = Contact.new(email: nil)
+    contact.valid?
+    expect(contact.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate email address" do
+    Contact.create(
+      firstname: 'Bret',
+      lastname: 'McKenzie',
+      email: 'fotc@email.com'
+      )
+    contact = Contact.new(
+      firstname: 'Jemaine',
+      lastname: 'Clement',
+      email: 'fotc@email.com')
+
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
+  end
+
+  it "returns a contact's full name as a string" do
+    contact = Contact.new(
+      firstname: 'Jemaine',
+      lastname: 'Clement',
+      email: 'fotc@email.com')
+
+    expect(contact.name).to eq("Jemaine Clement")
+  end
+
+end#describe
